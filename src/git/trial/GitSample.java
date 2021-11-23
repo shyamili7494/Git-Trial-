0f7aa2817a506c564b9570f817c9d9b76e2660c4
shyamili7494 <shyamili.rajan.99@gmail.com>
2021-11-24 01:14:54 +0530

    Project trial done successfully!

---
+++ src/git/trial/GitSample.java
@@ -0,0 +1,10 @@
+package git.trial;
+
+public class GitSample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git World!");
+		System.out.println("Rockstar is here!! ");
+	}
+}
